794b4128b940aa4784a2a501b36e0b5118da0541
VinodRadhakrishna <52576671+VinodAWSDevOps@users.noreply.github.com>
2024-09-01 00:20:07 +0530

    W1 D1 Assignment submitted

---
+++ Week 1 Day 1 Home Assignment/var_let_const.js
@@ -0,0 +1,16 @@
+//const browserVersion="chrome"
+function getBrowserVersion()
+{
+    let browserVersion="chrome"
+    if(browserVersion=="chrome")
+    {
+        console.log("Chorme browser")
+    }  
+    else
+    {
+        console.log("Not a chromebrowser")
+    }
+    console.log()
+}
+
+getBrowserVersion()
